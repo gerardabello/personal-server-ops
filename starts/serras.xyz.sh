7023a8e5e17553277e0b0e6833c8c8d812e47e50
Gerard Abelló Serras <gerardabello@gmail.com>
2017-07-18 20:31:11 +0200

    Remove published ports on serras

--- starts/serras.xyz.sh
+++ starts/serras.xyz.sh
@@ -5,6 +5,5 @@ docker run -d \
   --name=serras.xyz \
   --restart=always \
   --network=intranet \
-  -p 80:80 -p 443:443 \
   -v "${HOME}/volumes/serras.xyz/caddy-data:/root/.caddy" \
   gerardabello/serras.xyz

1f15252c943d25c25b151661ccedbbb43a3914ff
Gerard Abelló Serras <gerardabello@gmail.com>
2017-07-18 20:29:08 +0200

    Add serras to intranet network

--- starts/serras.xyz.sh
+++ starts/serras.xyz.sh
@@ -4,6 +4,7 @@ docker rm serras.xyz
 docker run -d \
   --name=serras.xyz \
   --restart=always \
+  --network=intranet \
   -p 80:80 -p 443:443 \
   -v "${HOME}/volumes/serras.xyz/caddy-data:/root/.caddy" \
   gerardabello/serras.xyz

ee26afb744202bd37fd1523ed9e57ec4ae484396
Gerard Abelló Serras <gerardabello@gmail.com>
2017-12-13 21:56:05 +0100

    Don't use link

--- starts/unwelch-backend.sh
+++ starts/unwelch-backend.sh
@@ -5,8 +5,7 @@ docker pull registry.gitlab.com/unwelch/backend
 docker run -d --name unwelch-backend \
   -p 2102:3000 \
   --restart=always \
-  --link unwelch-db:db \
-  -e DB_HOST="db" \
+  -e DB_HOST="serras.xyz" \
   -e DB_PORT="2200" \
   -e DB_USER="root" \
   -e DB_NAME="postgres" \

3a5dcd6c2638184c9ff3cbb9e04c4c9a91079e65
Gerard Abelló Serras <gerardabello@gmail.com>
2017-12-13 19:45:46 +0100

    Update unwelch

--- starts/unwelch-backend.sh
+++ starts/unwelch-backend.sh
@@ -5,6 +5,8 @@ docker run -d --name unwelch-backend \
   -p 2102:3000 \
   --restart=always \
   --link unwelch-db:db \
-  -e SQL_HOST="db" \
-  -e SQL_PORT="2200" \
+  -e DB_HOST="db" \
+  -e DB_PORT="2200" \
+  -e DB_USER="root" \
+  -e DB_NAME="postgres" \
   registry.gitlab.com/unwelch/backend

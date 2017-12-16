d24dbfa96222befc84ca052845a3a1549363b6ac
Gerard Abelló Serras <gerardabello@gmail.com>
2017-12-16 13:56:18 +0100

    Update unwelch db version

--- starts/unwelch-db.sh
+++ starts/unwelch-db.sh
@@ -7,5 +7,5 @@ docker run -d \
   --hostname=unwelch-db \
   -p 2200:26257 -p 2201:8080 \
   -v "${HOME}/volumes/unwelchdb/cockroach-data:/cockroach/cockroach-data" \
-  cockroachdb/cockroach:v1.0 \
+  cockroachdb/cockroach:v1.1.3 \
   start --insecure --cache=10MiB --max-sql-memory=10MiB

b3320b54839e462c9020c93436b191e99f84684b
Gerard Abelló <gerardabello@gmail.com>
2018-10-29 15:59:31 +0100

    feat: Update unwelch to github

--- starts/unwelch-backend.sh
+++ starts/unwelch-backend.sh
@@ -1,12 +1,13 @@
 docker stop unwelch-backend
 docker rm unwelch-backend
-docker pull registry.gitlab.com/unwelch/backend
+docker pull unwelch/backend
 
-docker run -d --name unwelch-backend \
+docker run -d \
+  --name=unwelch-backend \
   --restart=always \
   --network=intranet \
   -e DB_HOST="db.unwel.ch" \
   -e DB_PORT="2200" \
   -e DB_PASSWORD=$UNWELCH_DB_PASSWORD \
   -e DB_USER="postgres" \
-  registry.gitlab.com/unwelch/backend
+  unwelch/backend
--- starts/unwelch-frontend.sh
+++ starts/unwelch-frontend.sh
@@ -1,9 +1,9 @@
 docker stop unwelch-frontend
 docker rm unwelch-frontend
-docker pull registry.gitlab.com/unwelch/frontend
+docker pull unwelch/frontend
 
 docker run -d \
   --name=unwelch-frontend \
   --restart=always \
   --network=intranet \
-  registry.gitlab.com/unwelch/frontend
+  unwelch/frontend

748e488af29e457b9c7bb6494541a0d4224ec318
Gerard Abelló <gerardabello@gmail.com>
2019-01-18 15:36:46 +0100

    remove unwelch

--- starts/unwelch-frontend.sh
+++
@@ -1,9 +0,0 @@
-docker stop unwelch-frontend
-docker rm unwelch-frontend
-docker pull unwelch/frontend
-
-docker run -d \
-  --name=unwelch-frontend \
-  --restart=always \
-  --network=intranet \
-  unwelch/frontend
